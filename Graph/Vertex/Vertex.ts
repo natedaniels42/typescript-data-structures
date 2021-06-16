a3fb1fae1fb5e96276d513c762f67179fb2afba8
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-16 16:04:03 -0500

    Added print method to Vertex.ts

--- Graph/Vertex/Vertex.ts
+++ Graph/Vertex/Vertex.ts
@@ -16,6 +16,11 @@ class Vertex {
     removeEdge(vertex: Vertex): void {
         this.edges = this.edges.filter(edge => edge.end !== vertex);
     }
+
+    print(): void {
+        const edgeList: string[] = this.edges.map(edge => edge.weight ? `${edge.end.data} (${edge.weight})` : `${edge.end.data}`);
+        console.log(`${this.data} --> ${edgeList.join(', ')}`);
+    }
 }
 
 export default Vertex;
