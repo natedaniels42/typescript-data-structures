4bd0429134acacf9e3096702a99e54e21031909f
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-16 14:37:12 -0500

    Added assign method to HashMap.ts

--- HashMap/HashMap.js
+++ HashMap/HashMap.js
@@ -2,6 +2,7 @@
 exports.__esModule = true;
 var LinkedList_1 = require("../LinkedList/LinkedList");
 ;
+var Node_1 = require("../Node/Node");
 var HashMap = /** @class */ (function () {
     function HashMap(size) {
         this.hashMap = Array.apply(null, new Array(size)).map(function () { return new LinkedList_1["default"](); });
@@ -9,8 +10,41 @@ var HashMap = /** @class */ (function () {
     HashMap.prototype.hash = function (key) {
         return key.split('').map(function (char) { return char.charCodeAt(0); }).reduce(function (a, c) { return a + c; }) % this.hashMap.length;
     };
+    HashMap.prototype.assign = function (key, value) {
+        var arrayIndex = this.hash(key);
+        var list = this.hashMap[arrayIndex];
+        var current = list.head;
+        var previous = null;
+        if (!current) {
+            list.head = new Node_1["default"]({ key: key, value: value });
+        }
+        else {
+            while (current) {
+                if (current.data.key === key) {
+                    current.data.value = value;
+                    return;
+                }
+                previous = current;
+                current = current.getNextNode();
+            }
+            previous.setNextNode(new Node_1["default"]({ key: key, value: value }));
+        }
+    };
     return HashMap;
 }());
 var hm = new HashMap(5);
-console.log(hm);
-console.log(hm.hash('cat'));
+hm.assign('cat', 'felix');
+hm.assign('act', 'play');
+hm.assign('tac', 'tic');
+var current = hm.hashMap[2].head;
+while (current) {
+    console.log([current.data.key, current.data.value]);
+    current = current.getNextNode();
+}
+hm.assign('cat', 'Panthero');
+hm.assign('act', 'movie');
+current = hm.hashMap[2].head;
+while (current) {
+    console.log([current.data.key, current.data.value]);
+    current = current.getNextNode();
+}
--- HashMap/HashMap.ts
+++ HashMap/HashMap.ts
@@ -1,4 +1,5 @@
 import LinkedList from "../LinkedList/LinkedList";;
+import Node from "../Node/Node";
 
 class HashMap {
     hashMap: LinkedList[];
@@ -10,5 +11,26 @@ class HashMap {
     hash(key: string): number {
         return key.split('').map(char => char.charCodeAt(0)).reduce((a,c) => a + c) % this.hashMap.length;
     } 
+
+    assign(key: string, value: unknown): void {
+        const arrayIndex: number = this.hash(key);
+        const list: LinkedList = this.hashMap[arrayIndex];
+        let current: Node | null = list.head;
+        let previous: Node | null = null;
+        if (!current) {
+            list.head = new Node({key, value});
+        } else {
+            while (current) {
+                if (current.data.key === key) {
+                    current.data.value = value;
+                    return;
+                }
+                previous = current;
+                current = current.getNextNode();
+            }
+            previous.setNextNode(new Node({key, value}));
+        }
+    }
 }
+
 
