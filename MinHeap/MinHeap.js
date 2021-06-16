c40528da5c3a013336814b1e9ab4d0a1b844fd28
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-16 15:31:21 -0500

    Added exists, isEmpty, swap, and canSwap methods to MinHeap.ts

--- MinHeap/MinHeap.js
+++ MinHeap/MinHeap.js
@@ -3,6 +3,19 @@ var MinHeap = /** @class */ (function () {
         this.heap = [null];
         this.size = 0;
     }
+    MinHeap.prototype.canSwap = function (current, left, right) {
+        return (this.exists(left) && this.heap[left] < this.heap[current]) || (this.exists(right) && this.heap[right] < this.heap[current]);
+    };
+    MinHeap.prototype.swap = function (a, b) {
+        var _a;
+        _a = [this.heap[b], this.heap[a]], this.heap[a] = _a[0], this.heap[b] = _a[1];
+    };
+    MinHeap.prototype.isEmpty = function () {
+        return this.size === 0;
+    };
+    MinHeap.prototype.exists = function (current) {
+        return current <= this.size;
+    };
     MinHeap.prototype.getParent = function (current) {
         return Math.floor(current / 2);
     };
--- MinHeap/MinHeap.ts
+++ MinHeap/MinHeap.ts
@@ -7,6 +7,22 @@ class MinHeap {
         this.size = 0;
     }
 
+    canSwap(current:number, left: number, right: number): boolean {
+        return (this.exists(left) && this.heap[left] < this.heap[current]) || (this.exists(right) && this.heap[right] < this.heap[current]);
+    }
+
+    swap(a: number, b: number): void {
+        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    }
+
+    isEmpty(): boolean {
+        return this.size === 0;
+    }
+
+    exists(current: number): boolean {
+        return current <= this.size;
+    } 
+
     getParent(current: number): number {
         return Math.floor(current / 2);
     }
